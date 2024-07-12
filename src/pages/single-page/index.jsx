ca34c728cc8c89e72327684a162f717ddcdfb274
avazjanovsardorbek01 <sardoravazzanov@gmail.com>
2024-07-13 04:47:19 +1000

    Exam-7

--- src/pages/single-page/index.jsx
+++ src/pages/single-page/index.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { get_product } from "../../service/products";
-import "./single.css";
 
 const SinglePage = () => {
   const { id } = useParams();
